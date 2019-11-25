25d1675409a3abbd6dfa035344a712584c0f4312
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-25 00:10:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,6 @@
 class Song
   attr_accessor :name, :artist, :genre
   
-  def initialize(name, artist, genre)
-    @self.name = name
-    @self.artist = artist
-    @self.genre = genre
-  end
-  
   def new(name, artist, genre)
     @name = name
     @artist = artist
