bbd0fde126b039b4e94914b25e96d591a0bdada4
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-11-25 00:14:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre
   
-  def new(name, artist, genre)
+  def initialize(name, artist, genre)
     puts "#{name} and #{artist} and #{genre}."
   end
   
